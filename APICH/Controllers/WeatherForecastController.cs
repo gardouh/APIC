56c8d54aeefc32026e26c4c56e63b9212a8181c7
gardouh <gardouhkhalil1@gmail.com>
2020-10-08 21:22:18 +0200

    Hello word

---
+++ APICH/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APICH.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            var i = 0;
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                id = index,
+                email = "email@email.fr",
+                username = Summaries[index],
+                UserToken = "XXXX",
+                password = index.ToString(),
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ APICH/WeatherForecast.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace APICH
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int id { get; set; }
+
+        public string username { get; set; }
+
+        public string email { get; set; }
+        public string password { get; set; }
+        public string UserToken { get; set; }
+
+    }
+}
